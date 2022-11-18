57c3aa6ab051b351cf9af58b3e2cc422f8eb262b
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2022-11-18 16:33:08 -0300

    commit

--- src/Rucula.Aplication/DTOs/LanguageRuculaDTO.cs
+++ src/Rucula.Aplication/DTOs/LanguageRuculaDTO.cs
@@ -2,10 +2,9 @@
 using System.ComponentModel.DataAnnotations;
 public class LanguageRuculaDTO
 {
-
-        public int Id { get; set; }
-        public string? Sintax { get;set; }
+        [Required]
+        public string? Code { get; set; }
         public string? Description { get;set; }
-        
+        public string? Description2 { get;set; }
 }
- 
+
