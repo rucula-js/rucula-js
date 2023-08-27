a127e7f23b6fef54827a5b061c68a435b69a9faf
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2023-08-27 02:59:35 -0300

    Adicionando correção de eventos Key Frame Line

--- src/elements/frame/TypeLine/FrameLineEvent.ts
+++ src/elements/frame/TypeLine/FrameLineEvent.ts
@@ -53,12 +53,15 @@ function crudLineQuadro(event:Event){
         (varttt as HTMLInputElement)?.focus()
     }
 
-    if (KeyEventGetIndex(0) == "Enter"){
+    if (KeyEventGetIndex(0) == undefined || KeyEventGetIndex(1) == undefined) return
+
+    if (KeyEventGetIndex(0) == "Control" && KeyEventGetIndex(1) ==  "Enter"){
         event.preventDefault();
         currentLineElement.after(addLine(inputTargetEvent));
     }
-    if (KeyEventGetIndex(0) == "Control"){
+    if (KeyEventGetIndex(0) == "0" && KeyEventGetIndex(1) == "Control"){
         event.preventDefault();
         removeLine(currentLineElement,inputTargetEvent);
     }
+    KeyEventClear();
 }
