a939074ac1512d636e7185d18c2d5070f4cc6d45
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2023-09-10 01:30:14 -0300

    melhorando index

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import { domCreateForm } from "./src/window/WindowFactory";
-import { initGlobalConfiguration } from "./src/global/GlobalConfig";
 import config from "./exemples/window.global.json";
-import input from './exemples/ordemServico.json'
+import input from './exemples/ordemServicoApiControl.json'
+import { createWindow } from "./src/window/WindowFactory"
+import { initGlobalConfiguration } from "./src/global/GlobalConfig"
 import "./public/style.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 (()=> {
     initGlobalConfiguration(config as any)
-    domCreateForm(input as any)   
+    createWindow(input as any,"js")   
 })() 
