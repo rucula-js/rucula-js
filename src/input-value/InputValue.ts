bb86227280b28a2ecc5941d53205cf0f3a1d8e03
--global <--global>
2023-10-30 22:01:30 -0300

    refatorando

--- src/input-value/InputValue.ts
+++ src/input-value/InputValue.ts
@@ -8,47 +8,47 @@ export function setValueInForm(obj:any, objectDto:string="", line:string = ""){
         objectDto = getThis()
     }
 
-    Object.keys(obj).forEach(key => {
+    for (let propert in obj){
         
-        if(Array.isArray(obj[key])){
+        if(Array.isArray(obj[propert])){
             
             let representation = new RepresentationField()
-            representation.objectDto = key;
+            representation.objectDto = propert;
             
             cleanFrame(representation)
 
             let frameLine = document.querySelector(`[data-objectdto="${representation.objectDto}"] tbody`)
 
-            obj[key].forEach((item:any,index:any) => {
+            obj[propert].forEach((item:any,index:any) => {
 
                 representation.lineNumber = index 
                 
                 const line = addLine(representation)
                 frameLine?.appendChild(line)
 
-                setValueInForm(item,key,`.${index}`)
+                setValueInForm(item,propert,`.${index}`)
             
             })
         }
-
-        if(obj[key] == null){
+        
+        if(obj[propert] == null){
             return;
         }
 
-        if (typeof obj[key] == "string" || typeof obj[key] == "number"){ 
+        if (typeof obj[propert] == "string" || typeof obj[propert] == "number"){ 
 
-            let atribute = `[set="${objectDto}.${key}${line}"]`;
+            let atribute = `[set="${objectDto}.${propert}${line}"]`;
             
             let input = document.querySelector(atribute) as HTMLInputElement;
             
             if(input){
 
-                input.value = obj[key]
+                input.value = obj[propert]
             } 
         }
         
-        if(typeof obj[key] == "object"){
-            setValueInForm(obj[key],key);
+        if(typeof obj[propert] == "object"){
+            setValueInForm(obj[propert],propert);
         }
-    });
+    }
 }
