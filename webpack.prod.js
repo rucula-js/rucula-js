c01d39904f3ca44f79df43e69b03a6234f202f77
reginaldo.marinho <reginaldo@test.com>
2023-11-24 23:02:46 -0300

    add config webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,41 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(common, {
-  mode: 'production'});
+
+module.exports = {
+  mode:'production',
+  entry: [
+    './index.ts',
+    './public/style.css',
+    ],
+    plugins: [
+      new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/},
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+  devtool: 'source-map',
+};
