9f2d6630680e7b975e36d9ccda7cffb376726509
reginaldo.marinho <reginaldo@test.com>
2024-05-07 00:32:42 -0300

    RUC-UP version 0.0.48

--- dist/rucula.js
+++ dist/rucula.js
@@ -772,7 +772,8 @@ let windowBaseDOM = (() => {
         openActionswindow();
         function calculateHeightRuculaWindow() {
             let offsetTop = Number(ruculaWindow.offsetTop);
-            ruculaWindow.style.height = `calc( 100% - ${offsetTop}px`;
+            let height = Number(window.innerHeight);
+            ruculaWindow.style.height = `${height - offsetTop}px`;
         }
     }
     function createNameWindow(name) {
