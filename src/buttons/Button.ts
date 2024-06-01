049ed2008dac502a0c065503c6b326b5488084f8
reginaldo.marinho <reginaldo@test.com>
2024-05-31 23:11:36 -0300

    Melhorando verificação de existencia de Botão Especifico

--- src/buttons/Button.ts
+++ src/buttons/Button.ts
@@ -92,10 +92,10 @@ export let buttonsDOM = (()=> {
             
             let buttons = button?.filter(c=> buttonIsNotDefault(c.target))
             
-            if(buttons?.length == 0){
-                ListRightButtons?.classList.add('r-display-none')
+            if(buttons?.length == 0 || buttons == undefined){
+                document.querySelector('.r-vertical-actions')?.classList.add('r-display-none')
             }
-
+            
             buttons?.forEach(b => {
                     
                 const li = document.createElement("li")
